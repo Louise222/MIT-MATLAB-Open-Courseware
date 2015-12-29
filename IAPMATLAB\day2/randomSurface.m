9eeb65bb891dcafe164876ea1963f569511fca96
Yingjie Cao <yjcao12@fudan.edu.cn>
2015-12-29 20:27:29 +0800

    updated file

--- IAPMATLAB\day2/randomSurface.m
+++
@@ -1,9 +0,0 @@
-Z0=rand(5,5);
-[X0,Y0]=meshgrid(1:5,1:5);
-[X1,Y1]=meshgrid(1:0.1:5,1:0.1:5);
-Z1=interp2(X0,Y0,Z0,X1,Y1,'cubic');
-surf(X1,Y1,Z1); colormap(hsv); shading(interp);
-hold(ax,'on');
-contour(X1,Y1,Z1,15); 
-colorbar;
-caxis([0 1]);
--- IAPMATLAB\day3/Q6/escapeVelocity.m
+++ IAPMATLAB\day3/Q6/escapeVelocity.m
@@ -2,7 +2,7 @@ function n=escapeVelocity(Z0,c,N)
 Z=Z0; R=abs(Z);
 n=1;
 while all(R<2,n<=N)
-    Z=Z^2+c;
+    Z=Z.^2+c;
     R=abs(Z);
     n=n+1;
 end
